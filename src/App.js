3bbf61f259b3f8321a0a1c21421dff3da8c9da02
Elisa <elisacastaneda0045@gmail.com>
2020-07-21 18:03:21 +0200

    create async functions for fetch the data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Cards, Chart, CountryPicker } from "./components"; //set up Api
 import styles from "./App.module.css";
+import { fetchData } from "./api";
+
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
