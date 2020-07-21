0f099f6670a976e561f89e1d53d1f2033182bc8f
Elisa <elisacastaneda0045@gmail.com>
2020-07-21 23:35:38 +0200

    chart setup

--- src/api/index.js
+++ src/api/index.js
@@ -10,4 +10,11 @@ export const fetchData = async () => {
 
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
-}
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    console.log(data); 
+  } catch (error) {}
+};
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,28 @@
-import React from "react"; //set up Api
-const Chart = () => {
-  return (
-  <h1>Chart</h1>
-  )
+import React, { useState, useEffect } from "react"; //set up Api
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
-}
+const Chart = () => {
+  const [dailyData, setDailyData] = useState({});
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    console.log(dailyData);
+
+    fetchAPI();
+  }, []);
+
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: "",
+        datasets: [{}, {}],
+      }}
+    />
+  ) : null;
+
+  return <h1>Chart</h1>;
+};
 export default Chart; //set up Api
